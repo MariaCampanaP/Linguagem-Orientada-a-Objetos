11e4b43be38e05ea5c762d7cc74daa6d1c20d078
Maria <mariacampana121@gmail.com>
2025-06-22 15:54:54 -0300

    Delete Atividade 07-04-2025 directory

--- Atividade 07-04-2025/Exercício 1/Main.java
+++
@@ -1,9 +0,0 @@
-package br.edu.unespar.heranca;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Transporte transporte = new Transporte(2);
-        System.out.println(transporte.toString());
-    }
-}
